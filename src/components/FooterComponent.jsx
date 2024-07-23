3998334265a9fec4fe0ae29bc281e46a7ae29073
Chidi Nwachukwu <cnwachukwu@gmail.com>
2024-07-22 23:32:57 -0400

    First commit

---
+++ src/components/FooterComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div>
+        <footer className='footer'>
+            <p>Copyrights reserved at 2024-25 by CySoft</p>
+        </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
